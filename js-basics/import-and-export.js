ff0f4f40f558720fa44781eafeff455cba0350c5
JohannesStroebele <stroebele.j@gmail.com>
2021-03-18 20:21:39 +0100

    Added explanation for import and export

---
+++ js-basics/import-and-export.js
@@ -0,0 +1,30 @@
+// A) EXPORT
+// Used for exporting data in a file for usage somewhere else
+
+// 1) Export before declarations
+export const getSubtotalPrice = () => {}
+export const getBackgroundColorStyleForButton = () => {}
+
+// 2) Export apart from declarations
+// AND use export "as"
+const getMinimalPrice = () => {}
+const getBackgroundColorStyleForHeader = () => {}
+export {
+    getMinimalPrice,
+    getBackgroundColorStyleForHeader as getStyleHeader
+};
+
+// B) IMPORT
+// Used for importing data into a file from somewhere else
+// AND Import "as"
+import {getSubtotalPrice as getPrice, getBackgroundColorStyleForHeader} from './import-and-export.js'
+
+getPrice();
+
+// C) EXPORT DEFAULT AND IMPORT
+// Is only possible once per file!!!
+// If export default is chosen, the name of the function or variable to be imported can be chosen freely like with "as" in a
+export default getMinimalPrice;
+
+import getMinPrice from './import-and-export.js'
+getMinPrice();
