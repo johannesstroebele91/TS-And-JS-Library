d34ead304f7256f4941dfeb29ea0350e9dfc2856
Johannes Ströbele <stroebele.j@gmail.com>
2021-04-26 16:21:31 +0200

    Added explanation for restructuring arrays and object as well as restructuring files

---
+++ js-basics/variables/changing-arrays-and-obj/change-with-destruction.js
@@ -0,0 +1,54 @@
+/* Array Destructuring
+ * Object destructuring 
+ * Further example */
+
+// Destructuring
+// Easily extract array elements or object properties 
+// AND store them in variables
+
+// Data
+const userDetails = {
+    name: 'Peter Purtan',
+    address: {
+        city: 'malolos',
+        province: 'bulacan',
+        country: 'PH'
+    },
+    runFunction: () => {},
+    undef: undefined
+  }
+  
+// 1) Array Destructuring
+// Order defines which property gets assigned
+console.log("1) Array Destructuring")
+const numbers = [1,2,3];
+const [a, b] = numbers;
+console.log(a) // Hello
+console.log(b) // Max
+console.log(numbers);
+
+// Leaving out a number
+const names = ["Max","Peter","Sarah"];
+[name1, , name3] = names;
+console.log(name1);
+console.log(name3);
+
+// 2) Object destructuring
+// Name of the property defines which property gets assigned
+console.log("\n2) Object destructuring")
+const user = {
+    id: 42,
+    name: 'Max',
+    is_verified: true
+};
+const {id, is_verified} = user;
+console.log(id);
+console.log(is_verified);
+
+// 3) Further example
+console.log('\n3) Further example')
+const { name = 'Anh', address, phone: newPhone = '004837272817', ...otherDetails} = userDetails ;
+const [firstFruitDestructed = 'defaultFruit', ...otherFruitsDestructed] = fruits;
+console.log(name + ' ' + address.city + ' ' + address.country);
+console.log(newPhone);
+console.log(firstFruitDestructed + ',' + otherFruitsDestructed);
--- js-basics/variables/changing-arrays-and-obj/changing-without-desctrubturing.js
+++ js-basics/variables/changing-arrays-and-obj/changing-without-desctrubturing.js
@@ -1,11 +1,41 @@
+// ACCESS, ASSIGN, AND CHANGE ARRAYS AND OBJECTS WITHOUT DESTRUCTURING
+
+/* Basics
+ * SPREAD OPERATOR
+ * REST OPERATOR */ 
+
+// Data
+const userDetails = {
+  name: 'Peter Purtan',
+  address: {
+      city: 'malolos',
+      province: 'bulacan',
+      country: 'PH'
+  },
+  runFunction: () => {},
+  undef: undefined
+}
+
+// 1) Basics
+console.log("1) Basics");
+const fruits = ['Avocado', 'Banana', 'Apple'];
+
+const nameWithoutDestruction = userDetails.name;
+const addressWithoutDestruction = userDetails.address;
+console.log('Name: ' + nameWithoutDestruction);
+console.log('Address: ' + JSON.stringify(addressWithoutDestruction));
+
+const fristFruit = fruits[0] || 'defaultFruit';
+console.log('Frist fruit: ' + fristFruit);
+
 // 2) SPREAD OPERATOR
 // used to add up array elements OR object properties
-console.log("1) Spread Operator");
-console.log("1.1) Arrays");
+console.log("2) Spread Operator");
+console.log("2.1) Arrays");
 const oldArray = [1, 2, 3];
 const newArray = [...oldArray, 4, 5];
 console.log(oldArray.toString(), newArray.toString());
-console.log("1.2) Objects");
+console.log("2.2) Objects");
 const oldObj = {
   name: "Mattie",
   age: 6,
@@ -16,16 +46,17 @@ console.log(JSON.stringify(newObj));
 
 // 3) REST OPERATOR
 // Used to merge a list of arguments into an array
+console.log("3) REST Operator");
 
 // 3.1) For Functions, the rest operator allows to accept
 // an indefinite number of arguments as an array
-console.log("2) REST Operator");
 function sortArgs(...args) {
   return args.sort();
 }
 function lengthArray(...someArgs) {
     return someArgs.length; 
 }
+
 
 // 3.2) Arrow function
 // checks if an element in the array matches 1 and return it
