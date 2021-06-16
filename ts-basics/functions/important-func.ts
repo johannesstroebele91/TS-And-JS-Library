c317482e910ed31690d1888aeb003050c964288a
johannesstroebele91 <stroebele.j@gmail.com>
2021-06-16 17:36:08 +0200

    Fix compiling of TS using Quokka and added explanation for push vs concat function

--- ts-basics/functions/important-func.ts
+++ ts-basics/functions/important-func.ts
@@ -2,84 +2,115 @@
 
 // Variables for later usage
 let someRandomNumbers: number[] = [1, 2, 3, 4, 5];
-let monthsArray: string[] = ["January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"];
+let monthsArray: string[] = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 const inventoryObject = [
-    {name: 'apples', quantity: 2},
-    {name: 'bananas', quantity: 0},
-    {name: 'cherries', quantity: 5}
+  { name: "apples", quantity: 2 },
+  { name: "bananas", quantity: 0 },
+  { name: "cherries", quantity: 5 },
 ];
 
-class Employee {
-    name: string;
+class Person {
+  name: string;
 
-    constructor(name: string) {
-        this.name = name;
-    }
+  constructor(name: string) {
+    this.name = name;
+  }
 }
 
 // Array.from()
 // Enables to iterate over iterable variables such as arrays and objects
-console.log('Array.from()');
-console.log(Array.from(someRandomNumbers, value => value * 2)); // [2, 4, 6, 8, 10]
+console.log("Array.from()");
+console.log(Array.from(someRandomNumbers, (value) => value * 2)); // [2, 4, 6, 8, 10]
 
 // .forEach()
 // Find element in array
 // Executes a provided function once for every element in the array
-console.log('someArray.forEach()');
-console.log(monthsArray.forEach(months => console.log(months)));
+console.log("someArray.forEach()");
+console.log(monthsArray.forEach((months) => console.log(months)));
 
 // .find()
 // Find element in array
 // Returns the value of the first element in the array
 // that satisfies the provided test condition
-console.log('.find()');
+console.log(".find()");
 
-function findCherries(fruit: { name: string; }) {
-    return fruit.name === 'cherries';
+function findCherries(fruit: { name: string }) {
+  return fruit.name === "cherries";
 }
 console.log(inventoryObject.find(findCherries)); // { name: 'cherries', quantity: 5 }
-console.log(inventoryObject.find(element => element.name === 'apples')); // { name: 'apples', quantity: 2 }
+console.log(inventoryObject.find((element) => element.name === "apples")); // { name: 'apples', quantity: 2 }
 
 // .filter()
 // Filter elements in array
 // creates a new array with all the elements
 // that satisfies the provided test condition
-console.log('.filter()');
+console.log(".filter()");
 function isBigEnough(element: number, index: number, array: number[]) {
-    return (element >= 10);
+  return element >= 10;
 }
 
 let passed = [12, 5, 8, 130, 44].filter(isBigEnough);
-console.log("Test Value : " + passed );
-
+console.log("Test Value : " + passed);
 
 // Timeout
 // Create alert after 1000 ms
-console.log('setTimeout()');
-console.log(setTimeout(() => console.log('Hello'), 1000));
+console.log("setTimeout()");
+console.log(setTimeout(() => console.log("Hello"), 1000));
 
 // Create an element with an single Element in an Array
-console.log('Array.of()');
-console.log(Array.of(2348, 234,234).toString()); // Creates an array with the respective elements (most be the same type)
+console.log("Array.of()");
+console.log(Array.of(2348, 234, 234).toString()); // Creates an array with the respective elements (most be the same type)
 console.log(Array(4).toString()); // Creates an array with the length 4
 
 // Fill array with same element
-console.log('.fill()');
+console.log(".fill()");
 let someBooleanArray: boolean[] = new Array(5).fill(false);
 console.log(someBooleanArray);
 
 // .map()
 // executes a provided function once for every element in the array and
 // returns a new transformed array
-console.log('.map()');
+console.log(".map()");
 console.log(monthsArray); // Array before
-console.log(monthsArray.map(month => month.toUpperCase())); //
+console.log(monthsArray.map((month) => month.toUpperCase())); //
 console.log(monthsArray); // stay the same (not transformed)
 
 // .every()
-console.log('.every()');
+console.log(".every()");
 // tests whether all elements in the array pass the test implemented by the provided function
 const isBelowThreshold = (currentValue: number) => currentValue < 40;
 const array1 = [1, 30, 39, 29, 10, 13];
 console.log(array1.every(isBelowThreshold)); // output: true
+
+// .push() vs .concat()
+// concat() adds a new item to an array BUT creates a new array for this
+// instead of push()
+
+// push() adds elements to the end of an array and returns the new length of the array. Thus your return here is invalid.
+// The concat() method is used to merge arrays. Concat does not change the existing arrays, but instead returns a new array.
+let a1concat = [1];
+let b1concat = a1concat;
+let concatedItems = a1concat.concat([2]); // needs a declaration
+console.log("b1", a1concat);
+console.log("b2", b1concat);
+console.log("concatedItems", concatedItems);
+
+let a2push = [1];
+let b2push = a2push;
+a2push.push(2); // does NOT need declaration
+
+console.log("b1", a2push);
+console.log("b2", a2push);
--- ts-basics/typescript_how-to-compile.md
+++ ts-basics/typescript_how-to-compile.md
@@ -1,18 +1,21 @@
-# Instructions
+# 1) Prerequisites
 
-After installing TS (https://www.typescriptlang.org/download) it can be can be compiled as follows:
+1. Install TS (https://www.typescriptlang.org/download)
+2. `npm install typescript --save-devTypeScript`
 
-```npm
-npm install typescript --save-devTypeScript
-```
+# 2) Execution possibilities
+## 2.1) Run TS files using Quokka
+1. Install Quokka extension
+2. Set the keyboard shortcut of `Quokka.js: start on current file` to e.g. `option+shift++`
+3. Select the file and press shortcut
 
-## 1) Run TS files on the go
+## 2.2) Run TS files on the go
 
 1. Install npx
 2. Compile ts into js files using: Strg+Shift+B - `tsc: build -tsbasics/tsconfig.json`
 3. run npx ts-node nameOfTheFile.js (for the JS NOT TS FILE!)
 
-## 2) Use VS Extension
+## 2.3) Use VS Extension
 
 1. Create a tsconfig.json to the project (https://www.typescriptlang.org/docs/handbook/tsconfig-json.html#using-tsconfigjson-or-jsconfigjson)
    - add "target": "es2020" to the tsconfig.ts
@@ -21,13 +24,13 @@ npm install typescript --save-devTypeScript
 2. Compile the js file
 3. Afterwards just select a compiled js file and "run code"
 
-## 3) Node.js
+## 2.4) Node.js
 
 1. Create also a tsconfig.json
 2. Right click on the code of the respective ts file and choose "Compile TypeScript"
 3. Run JS file using the Node interpreter
 
-## Important
+# 3) Important
 
 - Files share their scope
   -> cannot declare a variable with the same name in different files
