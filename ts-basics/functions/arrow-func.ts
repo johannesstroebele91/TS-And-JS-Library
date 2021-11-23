f1ec7bb69b5aa32c21bd5ac9d8738f0ab9b2a8ab
johannesstroebele91 <stroebele.j@gmail.com>
2021-11-23 11:53:29 +0100

    Add additional explanation for duck typing and arrow functions

--- ts-basics/functions/arrow-func.ts
+++ ts-basics/functions/arrow-func.ts
@@ -4,17 +4,3 @@ let sum = (x: number, y: number): number => {
   return x + y;
 };
 console.log(sum(10, 20)); //returns 30
-
-let me = {
-  name: "Ashutosh Verma",
-  thisInArrow: () => {
-    const name = "Other name";
-
-    console.log("My name is " + name); // no 'this' binding here
-  },
-  thisInRegular() {
-    console.log("My name is " + this.name); // 'this' binding works here
-  },
-};
-me.thisInArrow();
-me.thisInRegular();
--- udemy-course_understanding-typescript/1_basics/0_project/types/duck-typing.ts
+++ udemy-course_understanding-typescript/1_basics/0_project/types/duck-typing.ts
@@ -1,8 +1,24 @@
-class Parrot {
-  constructor(public name: string, public sound: string) {}
+// “When I see a bird that walks like a duck and swims like a duck and quacks like a duck, I call that bird a duck.”
+// When two classes have the same properties
+// then TS does not care if the name of the class is different
+
+// IMPORTANT: each class does not
+
+class Cat {
+  constructor(public name: string, public nightvision: boolean) {}
 }
 class Duck {
-  constructor(public name: string) {}
+  constructor(public name: string, public sound: string) {}
 }
 
-const pepe = new Parrot("Pepe", "quack");
+class Pelican {
+  constructor(public name: string) {}
+}
+// Example for running fine
+// Works fine because both Duck and Pelican class have a name
+// IMPORTANT: additional properties like for Duck class ARE ok!
+const mira: Pelican = new Duck("Pepe", "quack");
+
+// Example for compile error
+// because cat and duck do not have the same types
+// const pepe: Cat = new Duck("Pepe", "quack"); // OUTPUT: Type 'Parrot' is not assignable to type 'Duck'. // Types of property 'sound' are incompatible.
